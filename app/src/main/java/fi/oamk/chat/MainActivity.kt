d289a87136e990e9ac316e778e7d6d222af8fe8c
Tobias Tögel <71888952+TobiTgl@users.noreply.github.com>
2021-03-24 17:07:06 +0200

    first commit

---
+++ app/src/main/java/fi/oamk/chat/MainActivity.kt
@@ -0,0 +1,152 @@
+package fi.oamk.chat
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = MainActivity::class.java.name
+    private lateinit var auth: FirebaseAuth
+    private var currentUser: FirebaseUser? = null
+    private lateinit var messages: ArrayList<String>
+    private lateinit var database: DatabaseReference
+    private lateinit var edMessage: EditText
+    private lateinit var rcMessageList: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edMessage = findViewById(R.id.messageText)
+        rcMessageList = findViewById(R.id.messageList)
+
+        database = Firebase.database.reference
+        auth = Firebase.auth
+
+        messages = arrayListOf()
+
+        edMessage.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP){
+                addMessage()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+
+        }
+
+        val messageListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.value != null) {
+                    val messagesFromDatabase =
+                        (snapshot.value as HashMap<*, ArrayList<String>>).get("messages")
+                    messages.clear()
+                    messagesFromDatabase?.forEach {
+                        if (it != null) messages.add(it)
+                    }
+                    rcMessageList.adapter?.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("Chat", error.toString())
+            }
+        }
+
+        database.addValueEventListener(messageListener)
+        rcMessageList.layoutManager = LinearLayoutManager(this)
+        rcMessageList.adapter = MyAdapter(messages)
+
+
+    }
+
+    fun addMessage() {
+        val newMessage = edMessage.text.toString()
+        messages.add(newMessage)
+        database.child("messages").setValue(messages)
+        edMessage.setText("")
+        closeKeyBoard()
+    }
+
+    private fun closeKeyBoard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        loginDialog()
+    }
+
+    fun loginDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        with(builder) {
+            setTitle("Log in")
+            val linearLayout: LinearLayout = LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
+            val inputEmail: EditText = EditText(this@MainActivity)
+            inputEmail.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint = "Enter email"
+            linearLayout.addView(inputEmail)
+
+            val inputPw: EditText = EditText(this@MainActivity)
+            inputPw.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputPw.hint = "Enter password"
+            linearLayout.addView(inputPw)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("OK") { dialog, which ->
+                login(inputEmail.text.toString(), inputPw.text.toString())
+            }.show()
+
+
+        }
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    Toast.makeText(
+                        baseContext, "Authentification successful",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    currentUser = auth.currentUser
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentification failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+}
